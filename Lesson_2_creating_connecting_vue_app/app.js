b88c530cce565f990ec136348524f31fd077c22e
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-28 10:04:04 +0800

    Change heading & paragraph

---
+++ Lesson_2_creating_connecting_vue_app/app.js
@@ -0,0 +1,9 @@
+const Counter = {
+    data() {
+      return {
+        heading: "This is a Heading",
+        paragraph: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, facilis quisquam? Quos dolorem delectus cum at, ducimus tenetur est fugit recusandae quam quaerat temporibus quo repellat fugiat commodi necessitatibus ipsa!"
+      }
+    }
+  }
+Vue.createApp(Counter).mount('#user-goal')
