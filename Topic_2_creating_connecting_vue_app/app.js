9c5fb00b239fca9cb7469b3720d446fd1383d738
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-28 10:49:31 +0800

    Topic 2 - Modified app.js

--- Topic_2_creating_connecting_vue_app/app.js
+++ Topic_2_creating_connecting_vue_app/app.js
@@ -4,6 +4,14 @@ const Counter = {
         heading: "This is a Heading",
         paragraph: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, facilis quisquam? Quos dolorem delectus cum at, ducimus tenetur est fugit recusandae quam quaerat temporibus quo repellat fugiat commodi necessitatibus ipsa!"
       }
+    },
+    methods: {
+      displayConsole() {
+        console.log(this.heading)
+      }
+    },
+    computed() {
+      console.log("test")
     }
   }
 Vue.createApp(Counter).mount('#user-goal')
